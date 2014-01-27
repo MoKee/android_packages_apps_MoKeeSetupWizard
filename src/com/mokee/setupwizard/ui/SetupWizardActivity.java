f0e6bceeab0b8ee5229623f12204b4768add1606
martincz <martincz@mokeedev.com>
2014-01-28 02:32:31 +0800

    MoKeeSetupWizard: update owner name
    We must refresh owner name
    
    Change-Id: I186ee458640d00fb616ddecf30dadac9ef01f4c3

--- src/com/mokee/setupwizard/ui/SetupWizardActivity.java
+++ src/com/mokee/setupwizard/ui/SetupWizardActivity.java
@@ -42,6 +42,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.UserHandle;
+import android.os.UserManager;
 import android.provider.Settings;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -316,6 +318,7 @@ public class SetupWizardActivity extends Activity implements SetupDataCallbacks 
     private void finishSetup() {
         Settings.Global.putInt(getContentResolver(), Settings.Global.DEVICE_PROVISIONED, 1);
         Settings.Secure.putInt(getContentResolver(), Settings.Secure.USER_SETUP_COMPLETE, 1);
+        UserManager.get(this).setUserName(UserHandle.myUserId(), getString(com.android.internal.R.string.owner_name));
         ((MKSetupWizard) AppGlobals.getInitialApplication()).enableStatusBar();
         Intent intent = new Intent("android.intent.action.MAIN");
         intent.addCategory("android.intent.category.HOME");
