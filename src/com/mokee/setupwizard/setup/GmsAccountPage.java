LineageOS/android_packages_apps_SetupWizard

SetupWizard: Move fragment to correct package
Change-Id: I5cf3a876f2574cbf2160715f972101f274c27c23

src/com/cyanogenmod/setupwizard/setup/GmsAccountPage.java
@@ -31,6 +31,7 @@ import android.os.Bundle;
 
 import com.cyanogenmod.setupwizard.R;
 import com.cyanogenmod.setupwizard.SetupWizardApp;
+import com.cyanogenmod.setupwizard.ui.LoadingFragment;
 import com.cyanogenmod.setupwizard.util.SetupWizardUtils;
 
 import java.io.IOException;

src/com/cyanogenmod/setupwizard/setup/WifiSetupPage.java
@@ -25,6 +25,7 @@ import android.os.Bundle;
 
 import com.cyanogenmod.setupwizard.R;
 import com.cyanogenmod.setupwizard.SetupWizardApp;
+import com.cyanogenmod.setupwizard.ui.LoadingFragment;
 import com.cyanogenmod.setupwizard.util.SetupWizardUtils;
 
 public class WifiSetupPage extends SetupPage {

src/com/cyanogenmod/setupwizard/ui/LoadingFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.setupwizard.setup;
+package com.cyanogenmod.setupwizard.ui;
 
 import com.cyanogenmod.setupwizard.R;
 import com.cyanogenmod.setupwizard.ui.SetupPageFragment;

MoKee/android_packages_apps_MoKeeSetupWizard

src/com/mokee/setupwizard/setup/GmsAccountPage.java
@@ -31,6 +31,7 @@ import android.os.Bundle;
 
 import com.mokee.setupwizard.R;
 import com.mokee.setupwizard.SetupWizardApp;
+import com.mokee.setupwizard.ui.LoadingFragment;
 import com.mokee.setupwizard.util.SetupWizardUtils;
 
 import java.io.IOException;

src/com/mokee/setupwizard/setup/WifiSetupPage.java
@@ -25,6 +25,7 @@ import android.os.Bundle;
 
 import com.mokee.setupwizard.R;
 import com.mokee.setupwizard.SetupWizardApp;
+import com.mokee.setupwizard.ui.LoadingFragment;
 import com.mokee.setupwizard.util.SetupWizardUtils;
 
 public class WifiSetupPage extends SetupPage {

src/com/mokee/setupwizard/ui/LoadingFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.mokee.setupwizard.setup;
+package com.mokee.setupwizard.ui;
 
 import com.mokee.setupwizard.R;
 import com.mokee.setupwizard.ui.SetupPageFragment;