LineageOS/android_packages_apps_SetupWizard

SetupWizard: Only show backup if a gms account was added
Change-Id: I6cadf141c330d072f8cfc51769e4f41b74ebf880

src/com/cyanogenmod/setupwizard/setup/OtherSettingsPage.java
@@ -37,6 +37,7 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.cyanogenmod.setupwizard.R;
+import com.cyanogenmod.setupwizard.SetupWizardApp;
 import com.cyanogenmod.setupwizard.ui.SetupPageFragment;
 import com.cyanogenmod.setupwizard.ui.WebViewDialogFragment;
 import com.cyanogenmod.setupwizard.util.SetupWizardUtils;
@@ -169,7 +170,9 @@ public class OtherSettingsPage extends SetupPage {
             }
             mBackupRow = mRootView.findViewById(R.id.backup);
             mBackupRow.setOnClickListener(mBackupClickListener);
-            mBackupRow.setVisibility(hasGms ? View.VISIBLE : View.GONE);
+            boolean backupVisible = hasGms &&
+                    SetupWizardUtils.accountExists(getActivity(), SetupWizardApp.ACCOUNT_TYPE_GMS);
+            mBackupRow.setVisibility(backupVisible ? View.VISIBLE : View.GONE);
             mBackup = (CheckBox) mRootView.findViewById(R.id.backup_checkbox);
             mLocationRow = mRootView.findViewById(R.id.location);
             mLocationRow.setOnClickListener(mLocationClickListener);

MoKee/android_packages_apps_MoKeeSetupWizard

Conflicts:
	src/com/mokee/setupwizard/setup/OtherSettingsPage.java

src/com/mokee/setupwizard/setup/OtherSettingsPage.java
@@ -37,6 +37,7 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.mokee.setupwizard.R;
+import com.mokee.setupwizard.SetupWizardApp;
 import com.mokee.setupwizard.ui.SetupPageFragment;
 import com.mokee.setupwizard.ui.WebViewDialogFragment;
 import com.mokee.setupwizard.util.SetupWizardUtils;
@@ -170,7 +171,9 @@ public class OtherSettingsPage extends SetupPage {
             }
             mBackupRow = mRootView.findViewById(R.id.backup);
             mBackupRow.setOnClickListener(mBackupClickListener);
-            mBackupRow.setVisibility(hasGms ? View.VISIBLE : View.GONE);
+            boolean backupVisible = hasGms &&
+                    SetupWizardUtils.accountExists(getActivity(), SetupWizardApp.ACCOUNT_TYPE_GMS);
+            mBackupRow.setVisibility(backupVisible ? View.VISIBLE : View.GONE);
             mBackup = (CheckBox) mRootView.findViewById(R.id.backup_checkbox);
             mLocationRow = mRootView.findViewById(R.id.location);
             mLocationRow.setOnClickListener(mLocationClickListener);