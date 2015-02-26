LineageOS/android_packages_apps_SetupWizard

SetupWizard: Refactor context type
Context is assumed to be SetupWizardActivity and blindly cast. Just make the Context reference a SetupWizardActivity.

Change-Id: I3d7ffd08980fbff0fbea51b45b28002b49813038

src/com/cyanogenmod/setupwizard/setup/AbstractSetupData.java
@@ -17,8 +17,9 @@
 package com.cyanogenmod.setupwizard.setup;
 
 import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.os.Bundle;
+
+import com.cyanogenmod.setupwizard.ui.SetupWizardActivity;
 
 import java.util.ArrayList;
 
@@ -26,13 +27,13 @@ public abstract class AbstractSetupData extends BroadcastReceiver implements Set
 
     private static final String TAG = AbstractSetupData.class.getSimpleName();
 
-    protected Context mContext;
+    protected SetupWizardActivity mContext;
     private ArrayList<SetupDataCallbacks> mListeners = new ArrayList<SetupDataCallbacks>();
     private PageList mPageList;
 
     private int mCurrentPageIndex = 0;
 
-    public AbstractSetupData(Context context) {
+    public AbstractSetupData(SetupWizardActivity context) {
         mContext = context;
         mPageList = onNewPageList();
     }

src/com/cyanogenmod/setupwizard/setup/DateTimePage.java
@@ -16,11 +16,6 @@
 
 package com.cyanogenmod.setupwizard.setup;
 
-import com.cyanogenmod.setupwizard.R;
-import com.cyanogenmod.setupwizard.ui.SetupPageFragment;
-
-import org.xmlpull.v1.XmlPullParserException;
-
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.DatePickerDialog;
@@ -46,6 +41,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.cyanogenmod.setupwizard.R;
+import com.cyanogenmod.setupwizard.ui.SetupPageFragment;
+import com.cyanogenmod.setupwizard.ui.SetupWizardActivity;
+
+import org.xmlpull.v1.XmlPullParserException;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -68,7 +69,7 @@ public class DateTimePage extends SetupPage {
     private static final int HOURS_1 = 60 * 60000;
 
 
-    public DateTimePage(Context context, SetupDataCallbacks callbacks) {
+    public DateTimePage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/cyanogenmod/setupwizard/setup/FinishPage.java
@@ -18,11 +18,11 @@ package com.cyanogenmod.setupwizard.setup;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.os.Bundle;
 
 import com.cyanogenmod.setupwizard.R;
 import com.cyanogenmod.setupwizard.ui.SetupPageFragment;
+import com.cyanogenmod.setupwizard.ui.SetupWizardActivity;
 
 public class FinishPage extends SetupPage {
 
@@ -30,7 +30,7 @@ public class FinishPage extends SetupPage {
 
     private FinishFragment mFinishFragment;
 
-    public FinishPage(Context context, SetupDataCallbacks callbacks) {
+    public FinishPage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/cyanogenmod/setupwizard/setup/MobileDataPage.java
@@ -34,13 +34,14 @@ import android.widget.TextView;
 
 import com.cyanogenmod.setupwizard.R;
 import com.cyanogenmod.setupwizard.ui.SetupPageFragment;
+import com.cyanogenmod.setupwizard.ui.SetupWizardActivity;
 import com.cyanogenmod.setupwizard.util.SetupWizardUtils;
 
 public class MobileDataPage extends SetupPage {
 
     public static final String TAG = "MobileDataPage";
 
-    public MobileDataPage(Context context, SetupDataCallbacks callbacks) {
+    public MobileDataPage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/cyanogenmod/setupwizard/setup/SimCardMissingPage.java
@@ -18,12 +18,12 @@ package com.cyanogenmod.setupwizard.setup;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.cyanogenmod.setupwizard.R;
 import com.cyanogenmod.setupwizard.ui.SetupPageFragment;
+import com.cyanogenmod.setupwizard.ui.SetupWizardActivity;
 
 public class SimCardMissingPage extends SetupPage {
 
@@ -33,7 +33,7 @@ public class SimCardMissingPage extends SetupPage {
     private static final int SIM_SIDE = 1;
     private static final int SIM_BACK = 2;
 
-    public SimCardMissingPage(Context context, SetupDataCallbacks callbacks) {
+    public SimCardMissingPage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/cyanogenmod/setupwizard/setup/WelcomePage.java
@@ -19,7 +19,6 @@ package com.cyanogenmod.setupwizard.setup;
 import android.app.ActivityOptions;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -32,6 +31,7 @@ import android.widget.NumberPicker;
 import com.cyanogenmod.setupwizard.R;
 import com.cyanogenmod.setupwizard.ui.LocalePicker;
 import com.cyanogenmod.setupwizard.ui.SetupPageFragment;
+import com.cyanogenmod.setupwizard.ui.SetupWizardActivity;
 
 import java.util.Locale;
 
@@ -41,7 +41,7 @@ public class WelcomePage extends SetupPage {
 
     private static final String ACTION_EMERGENCY_DIAL = "com.android.phone.EmergencyDialer.DIAL";
 
-    public WelcomePage(Context context, SetupDataCallbacks callbacks) {
+    public WelcomePage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/cyanogenmod/setupwizard/setup/WifiSetupPage.java
@@ -20,7 +20,6 @@ import android.app.Activity;
 import android.app.ActivityOptions;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -31,6 +30,7 @@ import android.util.Log;
 import com.cyanogenmod.setupwizard.R;
 import com.cyanogenmod.setupwizard.SetupWizardApp;
 import com.cyanogenmod.setupwizard.ui.LoadingFragment;
+import com.cyanogenmod.setupwizard.ui.SetupWizardActivity;
 import com.cyanogenmod.setupwizard.util.SetupWizardUtils;
 
 import java.io.IOException;
@@ -59,7 +59,7 @@ public class WifiSetupPage extends SetupPage {
     private Runnable mFinishCaptivePortalCheckRunnable = new Runnable() {
         @Override
         public void run() {
-            final Activity activity = (Activity)mContext;
+            final Activity activity = mContext;
             if (mIsCaptivePortal) {
                 try {
                     int netId = ConnectivityManager.from(activity)
@@ -90,7 +90,7 @@ public class WifiSetupPage extends SetupPage {
         }
     };
 
-    public WifiSetupPage(Context context, SetupDataCallbacks callbacks) {
+    public WifiSetupPage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
         String server = Settings.Global.getString(context.getContentResolver(), "captive_portal_server");
         if (server == null) server = DEFAULT_SERVER;
@@ -130,7 +130,7 @@ public class WifiSetupPage extends SetupPage {
     }
 
     @Override
-    public void doLoadAction(Activity context, int action) {
+    public void doLoadAction(SetupWizardActivity context, int action) {
         super.doLoadAction(context, action);
         SetupWizardUtils.launchWifiSetup(context);
     }

MoKee/android_packages_apps_MoKeeSetupWizard

Conflicts:
	src/com/cyanogenmod/setupwizard/setup/CyanogenServicesPage.java
	src/com/cyanogenmod/setupwizard/setup/CyanogenSettingsPage.java
	src/com/mokee/setupwizard/setup/ChooseDataSimPage.java
	src/com/mokee/setupwizard/setup/DateTimePage.java
	src/com/mokee/setupwizard/setup/FinishPage.java
	src/com/mokee/setupwizard/setup/MKSetupWizardData.java
	src/com/mokee/setupwizard/setup/MobileDataPage.java
	src/com/mokee/setupwizard/setup/OtherSettingsPage.java
	src/com/mokee/setupwizard/setup/SetupPage.java
	src/com/mokee/setupwizard/setup/SimCardMissingPage.java
	src/com/mokee/setupwizard/setup/WelcomePage.java
	src/com/mokee/setupwizard/setup/WifiSetupPage.java
	src/com/mokee/setupwizard/ui/LoadingFragment.java

src/com/mokee/setupwizard/setup/AbstractSetupData.java
@@ -17,8 +17,9 @@
 package com.mokee.setupwizard.setup;
 
 import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.os.Bundle;
+
+import com.mokee.setupwizard.ui.SetupWizardActivity;
 
 import java.util.ArrayList;
 
@@ -26,13 +27,13 @@ public abstract class AbstractSetupData extends BroadcastReceiver implements Set
 
     private static final String TAG = AbstractSetupData.class.getSimpleName();
 
-    protected Context mContext;
+    protected SetupWizardActivity mContext;
     private ArrayList<SetupDataCallbacks> mListeners = new ArrayList<SetupDataCallbacks>();
     private PageList mPageList;
 
     private int mCurrentPageIndex = 0;
 
-    public AbstractSetupData(Context context) {
+    public AbstractSetupData(SetupWizardActivity context) {
         mContext = context;
         mPageList = onNewPageList();
     }

src/com/mokee/setupwizard/setup/DateTimePage.java
@@ -16,11 +16,6 @@
 
 package com.mokee.setupwizard.setup;
 
-import com.mokee.setupwizard.R;
-import com.mokee.setupwizard.ui.SetupPageFragment;
-
-import org.xmlpull.v1.XmlPullParserException;
-
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.DatePickerDialog;
@@ -46,6 +41,12 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.mokee.setupwizard.R;
+import com.mokee.setupwizard.ui.SetupPageFragment;
+import com.mokee.setupwizard.ui.SetupWizardActivity;
+
+import org.xmlpull.v1.XmlPullParserException;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -68,7 +69,7 @@ public class DateTimePage extends SetupPage {
     private static final int HOURS_1 = 60 * 60000;
 
 
-    public DateTimePage(Context context, SetupDataCallbacks callbacks) {
+    public DateTimePage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/mokee/setupwizard/setup/FinishPage.java
@@ -21,7 +21,6 @@ import java.util.List;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.mokee.utils.MoKeeUtils;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -30,6 +29,7 @@ import android.view.inputmethod.InputMethodManager;
 
 import com.mokee.setupwizard.R;
 import com.mokee.setupwizard.ui.SetupPageFragment;
+import com.mokee.setupwizard.ui.SetupWizardActivity;
 import com.mokee.setupwizard.util.InputMethodItem;
 
 public class FinishPage extends SetupPage {
@@ -38,7 +38,7 @@ public class FinishPage extends SetupPage {
 
     private FinishFragment mFinishFragment;
 
-    public FinishPage(Context context, SetupDataCallbacks callbacks) {
+    public FinishPage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/mokee/setupwizard/setup/MobileDataPage.java
@@ -34,13 +34,14 @@ import android.widget.TextView;
 
 import com.mokee.setupwizard.R;
 import com.mokee.setupwizard.ui.SetupPageFragment;
+import com.mokee.setupwizard.ui.SetupWizardActivity;
 import com.mokee.setupwizard.util.SetupWizardUtils;
 
 public class MobileDataPage extends SetupPage {
 
     public static final String TAG = "MobileDataPage";
 
-    public MobileDataPage(Context context, SetupDataCallbacks callbacks) {
+    public MobileDataPage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/mokee/setupwizard/setup/SimCardMissingPage.java
@@ -18,12 +18,12 @@ package com.mokee.setupwizard.setup;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.mokee.setupwizard.R;
 import com.mokee.setupwizard.ui.SetupPageFragment;
+import com.mokee.setupwizard.ui.SetupWizardActivity;
 
 public class SimCardMissingPage extends SetupPage {
 
@@ -33,7 +33,7 @@ public class SimCardMissingPage extends SetupPage {
     private static final int SIM_SIDE = 1;
     private static final int SIM_BACK = 2;
 
-    public SimCardMissingPage(Context context, SetupDataCallbacks callbacks) {
+    public SimCardMissingPage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/mokee/setupwizard/setup/WelcomePage.java
@@ -19,7 +19,6 @@ package com.mokee.setupwizard.setup;
 import android.app.ActivityOptions;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -32,6 +31,7 @@ import android.widget.NumberPicker;
 import com.mokee.setupwizard.R;
 import com.mokee.setupwizard.ui.LocalePicker;
 import com.mokee.setupwizard.ui.SetupPageFragment;
+import com.mokee.setupwizard.ui.SetupWizardActivity;
 
 import java.util.Locale;
 
@@ -41,7 +41,7 @@ public class WelcomePage extends SetupPage {
 
     private static final String ACTION_EMERGENCY_DIAL = "com.android.phone.EmergencyDialer.DIAL";
 
-    public WelcomePage(Context context, SetupDataCallbacks callbacks) {
+    public WelcomePage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
     }
 

src/com/mokee/setupwizard/setup/WifiSetupPage.java
@@ -20,7 +20,6 @@ import android.app.Activity;
 import android.app.ActivityOptions;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -31,6 +30,7 @@ import android.util.Log;
 import com.mokee.setupwizard.R;
 import com.mokee.setupwizard.SetupWizardApp;
 import com.mokee.setupwizard.ui.LoadingFragment;
+import com.mokee.setupwizard.ui.SetupWizardActivity;
 import com.mokee.setupwizard.util.SetupWizardUtils;
 
 import java.io.IOException;
@@ -59,7 +59,7 @@ public class WifiSetupPage extends SetupPage {
     private Runnable mFinishCaptivePortalCheckRunnable = new Runnable() {
         @Override
         public void run() {
-            final Activity activity = (Activity)mContext;
+            final Activity activity = mContext;
             if (mIsCaptivePortal) {
                 try {
                     int netId = ConnectivityManager.from(activity)
@@ -90,7 +90,7 @@ public class WifiSetupPage extends SetupPage {
         }
     };
 
-    public WifiSetupPage(Context context, SetupDataCallbacks callbacks) {
+    public WifiSetupPage(SetupWizardActivity context, SetupDataCallbacks callbacks) {
         super(context, callbacks);
         String server = Settings.Global.getString(context.getContentResolver(), "captive_portal_server");
         if (server == null) server = DEFAULT_SERVER;
@@ -130,7 +130,7 @@ public class WifiSetupPage extends SetupPage {
     }
 
     @Override
-    public void doLoadAction(Activity context, int action) {
+    public void doLoadAction(SetupWizardActivity context, int action) {
         super.doLoadAction(context, action);
         SetupWizardUtils.launchWifiSetup(context);
     }