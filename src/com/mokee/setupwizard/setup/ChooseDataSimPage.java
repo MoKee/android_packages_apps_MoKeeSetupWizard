LineageOS/android_packages_apps_SetupWizard

SetupWizard: Don't assume order of SubInfoRecords
Change-Id: I3b4132afccc96a5d08986a3cb902bd384931f21f

src/com/cyanogenmod/setupwizard/setup/ChooseDataSimPage.java
@@ -81,7 +81,7 @@ public class ChooseDataSimPage extends SetupPage {
         private SparseArray<CheckBox> mCheckBoxes;
 
         private TelephonyManager mPhone;
-        private List<SubInfoRecord> mSubInfoRecords;
+        private SparseArray<SubInfoRecord> mSubInfoRecords;
         private SparseArray<SignalStrength> mSignalStrengths;
         private SparseArray<ServiceState> mServiceStates;
         private SparseArray<PhoneStateListener> mPhoneStateListeners;
@@ -102,8 +102,13 @@ public class ChooseDataSimPage extends SetupPage {
         @Override
         protected void initializePage() {
             mPageView = (ViewGroup)mRootView.findViewById(R.id.page_view);
-            mSubInfoRecords =  SubscriptionManager.getActiveSubInfoList();
-            int simCount = mSubInfoRecords.size();
+            List<SubInfoRecord> subInfoRecords =  SubscriptionManager.getActiveSubInfoList();
+            int simCount = subInfoRecords.size();
+            mSubInfoRecords = new SparseArray<SubInfoRecord>(simCount);
+            for (int i = 0; i < simCount; i++) {
+                SubInfoRecord subInfoRecord = subInfoRecords.get(i);
+                mSubInfoRecords.put(subInfoRecord.slotId, subInfoRecord);
+            }
             mNameViews = new SparseArray<TextView>(simCount);
             mSignalViews = new SparseArray<ImageView>(simCount);
             mCheckBoxes = new SparseArray<CheckBox>(simCount);

MoKee/android_packages_apps_MoKeeSetupWizard

src/com/mokee/setupwizard/setup/ChooseDataSimPage.java
@@ -77,7 +77,7 @@ public class ChooseDataSimPage extends SetupPage {
     }
 
 
-    public class ChooseDataSimFragment extends SetupPageFragment {
+    public static class ChooseDataSimFragment extends SetupPageFragment {
 
         private ViewGroup mPageView;
         private SparseArray<TextView> mNameViews;
@@ -85,7 +85,7 @@ public class ChooseDataSimPage extends SetupPage {
         private SparseArray<CheckBox> mCheckBoxes;
 
         private TelephonyManager mPhone;
-        private List<SubscriptionInfo> mSubInfoRecords;
+        private SparseArray<SubscriptionInfo> mSubInfoRecords;
         private SparseArray<SignalStrength> mSignalStrengths;
         private SparseArray<ServiceState> mServiceStates;
         private SparseArray<PhoneStateListener> mPhoneStateListeners;
@@ -107,8 +107,14 @@ public class ChooseDataSimPage extends SetupPage {
         @Override
         protected void initializePage() {
             mPageView = (ViewGroup)mRootView.findViewById(R.id.page_view);
-            mSubInfoRecords =  mSubscriptionManager.getActiveSubscriptionInfoList();
-            int simCount = mSubInfoRecords.size();
+            List<SubscriptionInfo> subInfoRecords =  SubscriptionController
+                    .getInstance().getActiveSubscriptionInfoList();
+            int simCount = subInfoRecords.size();
+            mSubInfoRecords = new SparseArray<SubscriptionInfo>(simCount);
+            for (int i = 0; i < simCount; i++) {
+                SubscriptionInfo subInfoRecord = subInfoRecords.get(i);
+                mSubInfoRecords.put(subInfoRecord.getSubscriptionId(), subInfoRecord);
+            }
             mNameViews = new SparseArray<TextView>(simCount);
             mSignalViews = new SparseArray<ImageView>(simCount);
             mCheckBoxes = new SparseArray<CheckBox>(simCount);