LineageOS/android_packages_apps_SetupWizard

SetupWizard: Add hook to finish setup for CTS automation
Change-Id: Ifd7edda671fd8011a9b9ed894333c292cd0c3331

src/com/cyanogenmod/setupwizard/setup/FinishSetupReceiver.java
@@ -1,0 +1,29 @@
+package com.cyanogenmod.setupwizard.setup;
+
+import android.app.StatusBarManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import com.cyanogenmod.setupwizard.SetupWizardApp;
+import com.cyanogenmod.setupwizard.util.SetupWizardUtils;
+
+public class FinishSetupReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (SetupWizardUtils.isDeviceLocked()) {
+            return;
+        }
+        Settings.Global.putInt(context.getContentResolver(), Settings.Global.DEVICE_PROVISIONED, 1);
+        Settings.Secure.putInt(context.getContentResolver(),
+                Settings.Secure.USER_SETUP_COMPLETE, 1);
+        ((StatusBarManager)context.getSystemService(Context.STATUS_BAR_SERVICE)).disable(
+                StatusBarManager.DISABLE_NONE);
+        Settings.Global.putInt(context.getContentResolver(),
+                SetupWizardApp.KEY_DETECT_CAPTIVE_PORTAL, 1);
+        SetupWizardUtils.disableGMSSetupWizard(context);
+        SetupWizardUtils.disableSetupWizard(context);
+    }
+}

MoKee/android_packages_apps_MoKeeSetupWizard

src/com/mokee/setupwizard/setup/FinishSetupReceiver.java
@@ -1,0 +1,29 @@
+package com.mokee.setupwizard.setup;
+
+import android.app.StatusBarManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import com.mokee.setupwizard.SetupWizardApp;
+import com.mokee.setupwizard.util.SetupWizardUtils;
+
+public class FinishSetupReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (SetupWizardUtils.isDeviceLocked()) {
+            return;
+        }
+        Settings.Global.putInt(context.getContentResolver(), Settings.Global.DEVICE_PROVISIONED, 1);
+        Settings.Secure.putInt(context.getContentResolver(),
+                Settings.Secure.USER_SETUP_COMPLETE, 1);
+        ((StatusBarManager)context.getSystemService(Context.STATUS_BAR_SERVICE)).disable(
+                StatusBarManager.DISABLE_NONE);
+        Settings.Global.putInt(context.getContentResolver(),
+                SetupWizardApp.KEY_DETECT_CAPTIVE_PORTAL, 1);
+        SetupWizardUtils.disableGMSSetupWizard(context);
+        SetupWizardUtils.disableSetupWizard(context);
+    }
+}