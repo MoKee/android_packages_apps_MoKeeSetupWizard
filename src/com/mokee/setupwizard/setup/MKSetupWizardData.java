6d73d7ac24d50d18d03cc7e74c3ef8031b3b8b99
cretin45 <cretin45@gmail.com>
2015-09-17 13:48:55 -0700

    SetupWizard: Tweak logic for showing/hiding sim missing page
    CYNGNOS-1021
    
    Change-Id: I6139ca4368eb1bf49b07a95ac60ca28831e2cd3a

--- src/com/mokee/setupwizard/setup/MKSetupWizardData.java
+++ src/com/mokee/setupwizard/setup/MKSetupWizardData.java
@@ -116,10 +116,14 @@ public class MKSetupWizardData extends AbstractSetupData {
     private void showHideSimMissingPage() {
         SimCardMissingPage simCardMissingPage =
                 (SimCardMissingPage) getPage(SimCardMissingPage.TAG);
-        if (simCardMissingPage != null && isSimInserted()) {
-            simCardMissingPage.setHidden(true);
-            if (isCurrentPage(simCardMissingPage)) {
-                onNextPage();
+        if (simCardMissingPage != null) {
+            if (isSimInserted()) {
+                simCardMissingPage.setHidden(true);
+                if (isCurrentPage(simCardMissingPage)) {
+                    onNextPage();
+                }
+            } else {
+                simCardMissingPage.setHidden(false);
             }
         }
     }
@@ -175,7 +179,8 @@ public class MKSetupWizardData extends AbstractSetupData {
                 continue;
             }
             if (state != TelephonyManager.SIM_STATE_ABSENT
-                    && state != TelephonyManager.SIM_STATE_UNKNOWN) {
+                    && state != TelephonyManager.SIM_STATE_UNKNOWN
+                    && state != TelephonyManager.SIM_STATE_NOT_READY) {
                  return true;
             }
         }
