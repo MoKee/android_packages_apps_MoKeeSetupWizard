aa92c7b7cd2ba57dfb097c77bf76bf1b1ccf4735
Danesh M <daneshm90@gmail.com>
2016-01-27 16:11:09 -0800

    SetupWizard : Use MccTable for mcc -> locale lookup
    Some sims such as T-Mobile return incorrect languages for their
    PL(Preferred languages). Lookup the locale via the MccTable instead,
    and fallback to trusting the sim.
    
    Change-Id: I612d4952355ce4a42b35579e1f9aa879417c4cd4

--- Android.mk
+++ Android.mk
@@ -31,7 +31,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v13 \
     play \
     libphonenumber \
-    org.mokee.platform.sdk
+    org.mokee.platform.sdk \
+    telephony-common
 
 # Include res dir from chips
 google_play_dir := ../../../external/google/google_play_services/libproject/google-play-services_lib/res
